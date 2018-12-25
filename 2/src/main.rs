de99108c57c7049dbc7e068bfca436fdd57aa541
Pascal Friederich <paukul@gmail.com>
2018-12-25 01:38:49 +0100

    slightly less awkward solution to 2b

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -8,7 +8,7 @@ use test::Bencher;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("could not read input");
-    let ids = input.split('\n').collect::<Vec<&str>>();
+    let ids = input.lines().collect::<Vec<&str>>();
     let result = sum(&ids);
     println!("2.a: {:?}", result);
     println!("2.b: {:?}", find_partial(ids));
@@ -32,7 +32,7 @@ fn calc(ids: &str) -> (i32, i32) {
     })
 }
 
-fn sum(ids: &Vec<&str>) -> i32 {
+fn sum(ids: &[&str]) -> i32 {
     let sums = ids
         .iter()
         .map(|id| calc(id))
@@ -42,31 +42,37 @@ fn sum(ids: &Vec<&str>) -> i32 {
 
 #[allow(dead_code)]
 fn similars(ids: Vec<&str>) -> (&str, &str) {
-    ids.iter().enumerate().find_map(|(index, &id)| {
-        let head = &ids[0..index];
-        let tail = &ids[(index + 1)..];
+    ids.iter()
+        .enumerate()
+        .find_map(|(index, &id)| {
+            let head = &ids[0..index];
+            let tail = &ids[(index + 1)..];
 
-        let found = head.iter().chain(tail.iter()).find(|&comparison_id| {
-            let diff = comparison_id.chars().zip(id.chars()).fold(0, |mut diff, (left, right)| {
-                if left != right {
-                    diff += 1;
-                }
-                diff
+            let found = head.iter().chain(tail.iter()).find(|&comparison_id| {
+                let diff =
+                    comparison_id
+                        .chars()
+                        .zip(id.chars())
+                        .fold(0, |mut diff, (left, right)| {
+                            if left != right {
+                                diff += 1;
+                            }
+                            diff
+                        });
+
+                diff == 1
             });
-
-            diff == 1
-        });
-        found.map(|&f| (id, f))
-    }).unwrap()
+            found.map(|&f| (id, f))
+        })
+        .unwrap()
 }
 
-fn same_partial<'a, 'b>(left: &'a str, right: &'b str) -> String {
-    left.chars().zip(right.chars()).fold(String::new(), |mut same, (l, r)| {
-        if l == r {
-            same.push(l);
-        }
-        same
-    }).trim().to_owned()
+fn same_partial(left: &str, right: &str) -> String {
+    left.chars()
+        .zip(right.chars())
+        .filter(|&(l, r)| l == r)
+        .map(|(c, _)| c)
+        .collect()
 }
 
 fn find_partial(ids: Vec<&str>) -> String {
@@ -90,7 +96,7 @@ fn test_suming_up() {
     let seq = vec![
         "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab",
     ];
-    assert_eq!(sum(seq), 12);
+    assert_eq!(sum(&seq), 12);
 }
 
 #[test]
